8d11f17c882e8598b3293be5b04205c0ae84e4ec
ravi2001c <121542590+ravi2001c@users.noreply.github.com>
2026-02-10 15:47:32 +0530

    Valentine project ❤️

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'valentine';
+  noX = 300;
+  noY = 300;
+  accepted = false;
+
+  burstArray = new Array(20);
+
+  moveNo() {
+    this.noX = Math.random() * (window.innerWidth - 120);
+    this.noY = Math.random() * (window.innerHeight - 60);
+  }
+
+  accept() {
+    this.accepted = true;
+  }
 }
